dc8fa9e9d9eaccf28f6dedd73c66a4bc9e516076
CloudDataEngineering <CloudDataEngineering>
2024-06-17 14:58:56 +0000

    ok

---
+++ Formula1-Project-Solutions/Section-21/includes/configuration.py
@@ -0,0 +1,10 @@
+# Databricks notebook source
+raw_folder_path = "/mnt/adlsacde/raw"
+processed_folder_path = "/mnt/adlsacde/processed"
+presentation_folder_path = "/mnt/adlsacde/presentation"
+
+# COMMAND ----------
+
+#raw_folder_path = 'abfss://raw@formula1dl.dfs.core.windows.net'
+#processed_folder_path = 'abfss://processed@formula1dl.dfs.core.windows.net'
+#presentation_folder_path = 'abfss://presentation@formula1dl.dfs.core.windows.net'
