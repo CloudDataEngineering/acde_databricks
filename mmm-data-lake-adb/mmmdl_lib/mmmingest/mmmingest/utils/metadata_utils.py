da3d39a73e28d35ad03c019107c988384ffc4f45
CloudDataEngineering <CloudDataEngineering>
2024-06-20 04:55:49 +0000

    ok

--- mmm-data-lake-adb/mmmdl_lib/mmmingest/mmmingest/utils/metadata_utils.py
+++ mmm-data-lake-adb/mmmdl_lib/mmmingest/mmmingest/utils/metadata_utils.py
@@ -9,3 +9,25 @@ class MetadataUtils:
     def get_table_config(self):
         if self.source_system_name == "mmmplan" or "controller":
             table_config = self.spark.sql(f"select * from mmm_metadate.mmm_tables").where(
+                (F.col("source_system_name") == self.source_system_name) & (
+                        F.col("source_object_type") == self.source_object_type) & (
+                        F.col("source_path_name") == self.source_path_name) & (
+                        F.col("source_object_name") == self.source_object_name)).collect()
+            if not table_config:
+                raise Exception("No entry found in the metadate config table for the processed object")
+            return table_config[0]
+        else:
+            table_congig = self.spark.sql(f"select * from mmm_metadate.mmm_tables").where(
+                (F.col("source_system_name") == self.source_system_name) & (
+                        F.col("source_object_type") == self.source_object_type) & (
+                        F.col("source_path_name") == self.source_path_name) & (
+                        F.col("source_schema_name") == self.source_schema_name) & (
+                        F.col("source_object_name") == self.source_object_name)).collect()
+            
+    def get_column_config(self):
+        if self
+            
+
+
+
+
